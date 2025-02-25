6cc435f0e3de99ae2af908134cde7411fe9442d1
maymolrobert <31981481+maymolrobert@users.noreply.github.com>
2025-02-24 21:49:07 -0500

    Added test for Equilateral Triangle

---
+++ TriangleSolverTest/TriangleTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using TriangleSolver; // Reference the main project
+
+namespace TriangleSolverTest
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+        // 1. Valid Equilateral Triangle
+        [Test]
+        public void AnalyzeTriangle_EquilateralTriangle_ReturnsEquilateral()
+        {
+            var result = Triangle.AnalyzeTriangle(5, 5, 5);
+            Assert.That(result, Is.EqualTo("Equilateral triangle"));
+        }
+
+    }
+}
